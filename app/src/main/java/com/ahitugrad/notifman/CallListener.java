cffbe7fcd038b005247680ecfb08ebb8b525257e
mazizulak <mazula95@gmail.com>
2017-05-24 12:30:37 +0300

    call duration is obtaining

--- app/src/main/java/com/ahitugrad/notifman/CallListener.java
+++ app/src/main/java/com/ahitugrad/notifman/CallListener.java
@@ -16,6 +16,8 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by maziz on 20.05.2017.
@@ -24,7 +26,6 @@ import java.util.List;
 public class CallListener extends PhoneStateListener {
     Context mContext;
     public static String LOG_TAG = "CallListener";
-    private Cursor managedCursor;
 
     public CallListener(Context context) {
         mContext = context;
@@ -37,29 +38,33 @@ public class CallListener extends PhoneStateListener {
         switch (state) {
             case TelephonyManager.CALL_STATE_IDLE:
                 Log.i(LOG_TAG, "onCallStateChanged: CALL_STATE_IDLE");
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (ActivityCompat.checkSelfPermission(mContext, android.Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        Cursor c = mContext.getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, null);
+                        int duration = c.getColumnIndex(CallLog.Calls.DURATION);// for duration
+                        int number = c.getColumnIndex(CallLog.Calls.NUMBER);
+                        c.moveToLast();
+                        //while (c.moveToNext()){
+                            String sure = c.getString(duration);
+                            String phNumber = c.getString(number);
+                            Log.v("Callog Duration: ",sure);
+                            Log.v("Call Number: ", phNumber);
+                        //}
 
-                if (ActivityCompat.checkSelfPermission(mContext, android.Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-
-                    return;
-                }
-                try {
-                    Cursor c = mContext.getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, null);
-                    String num= c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));// for  number
-                    String name= c.getString(c.getColumnIndex(CallLog.Calls.CACHED_NAME));// for name
-                    String duration = c.getString(c.getColumnIndex(CallLog.Calls.DURATION));// for duration
-                    Log.v("Callog Duration: ",duration);
-                }catch (Exception e){
-
-                }
-
-
+                    }
+                },4 * 1000);
 
 
                 break;
